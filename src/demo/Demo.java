472f70945de0dd258c4ae1f42c38d6e06f9299f4
senet <senet@10.0.0.22>
2023-07-30 20:14:51 -0400

    Implemented add ()

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,15 @@
 package demo;
 
 public class Demo {
+	public static void main(String[] args) {
+		System.out.println("yo");
+		
+		
+		
+	}
+	
+	public void add (int a , int b) {
+		System.out.println(a+b);
+	}
 
 }
